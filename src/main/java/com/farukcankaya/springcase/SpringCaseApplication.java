9385132fefe060c105da4c496d222c55a5722b5a
Faruk Cankaya <farukcankayaaa@gmail.com>
2018-05-18 17:37:05 +0300

    init

---
+++ src/main/java/com/farukcankaya/springcase/SpringCaseApplication.java
@@ -0,0 +1,12 @@
+package com.farukcankaya.springcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCaseApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringCaseApplication.class, args);
+  }
+}
---
+++ src/test/java/com/farukcankaya/springcase/SpringCaseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.farukcankaya.springcase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
