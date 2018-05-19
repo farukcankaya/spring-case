936533f5006cea92d131db9ad37c7b198bc314c2
Faruk Cankaya <farukcankayaaa@gmail.com>
2018-05-19 08:18:05 +0300

    setup test environment

--- src/test/java/com/farukcankaya/springcase/SpringCaseApplicationTests.java
+++ src/test/java/com/farukcankaya/springcase/SpringCaseApplicationTests.java
@@ -6,11 +6,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(
+  webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+  classes = SpringCaseApplication.class
+)
 public class SpringCaseApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
-
+  @Test
+  public void contextLoads() {}
 }
